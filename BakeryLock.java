d12f20fa556934f24426812d9375f04ce68a82ea
Showlon <u25502469@tuks.co.za>
2026-08-22 20:43:57 +0200

    Correction made to the mis-use of the Volatile classes

--- BakeryLock.java
+++ BakeryLock.java
@@ -7,24 +7,25 @@ public class BakeryLock implements Lock
 
     public BakeryLock(int n) 
     {
-        flag = new VolatileBoolean(n);
-        label =new VolatileInt(n);
+        this.n = n;
+        flag = new VolatileBoolean[n];
+        label =new VolatileInt[n];
         for(int i = 0; i < n; i++){
-            flag[i] = false;
-            label[i] = 0;
+            flag[i] = VolatileBoolean(false);
+            label[i] = VolatileInt(0);
         }
     }
 
     @Override
     public void lock(int threadId) 
     {
-        flag[threadId] = true;
-        label[threadId] = max(label);
+        flag[threadId].value = true;
+        label[threadId].label = max(label);
         for(int j = 0; j < n;j++){
-            if(j == i){
+            if(j == threadId){
                 continue;
             }
-            while(flag[j] && (label[j] < label[i] || (label[j] == label[i] && j < i))){
+            while(flag[j] && (label[j] < label[threadId] || (label[j] == label[threadId] && j < threadId))){
                 //wait
             }
         }
@@ -33,14 +34,14 @@ public class BakeryLock implements Lock
     @Override
     public void unlock(int threadId) 
     {
-        flag[threadId] = false;
+        flag[threadId].value = false;
     }
 
     public int max(VolatileInt[] a){
         int max = 0;
         for(int i = 0; i < n;i++){
-            if(a[i] > max){
-                max = a[i];
+            if(a[i].value > max){
+                max = a[i].value;
             }
         }
         return max + 1;
